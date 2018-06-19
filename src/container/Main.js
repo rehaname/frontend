b52af6fa568711622e354c02674d03d72927f9bc
Ziegfreid Morissey Flameño <ziegmoris@gmail.com>
2018-06-19 22:47:11 +0800

    Initial commit

---
+++ src/container/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Button from 'antd/lib/button';
+import axios from 'axios'
+
+class Main extends Component {
+    click() {
+        axios.get("http://localhost:8080/sample").then(res => {
+            console.log(res);
+            alert("Received Successful response from server!");
+            this.setState({clickMessage: res.data});
+        }, err => {
+            alert("Server rejected response with: " + err);
+        });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {clickMessage: 'Not click :('}
+
+        this.click = this.click.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="Main">
+                <header className="App-header">
+                    <h1 className="App-title">GSP ITS Passion Project</h1>
+                </header>
+                <p className="App-intro">
+                    <div>
+                        <Button onClick={this.click} type="primary">Button</Button>
+                        <div>Clicked?: {this.state.clickMessage}</div>
+                    </div>
+                </p>
+            </div>
+        );
+    }
+}
+export default Main;
