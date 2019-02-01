6b781a654b2ea3e0372507846def7ff3dc998574
Ana Katrina De Leon <ana.katrina.de.leon@oocl.com>
2019-02-01 11:23:16 +0800

    display random pairs in table

--- src/container/Pairing/PairingActions.js
+++ src/container/Pairing/PairingActions.js
@@ -1,5 +1,6 @@
 export const ADD_NEW_MATRIX = "ADD_NEW_MATRIX";
 export const MAP_MEMBERS_TO_TABLE = "MAP_MEMBERS_TO_TABLE";
+export const MAP_PAIRS_TO_TABLE = "MAP_PAIRS_TO_TABLE";
 
 export const addNewMatrix = (data) => ({
     type: ADD_NEW_MATRIX,
@@ -8,5 +9,10 @@ export const addNewMatrix = (data) => ({
 
 export const mapMembersToTable = (data) => ({
     type: MAP_MEMBERS_TO_TABLE,
+    data
+});
+
+export const mapPairsToTable = (data) => ({
+    type: MAP_PAIRS_TO_TABLE,
     data
 });
